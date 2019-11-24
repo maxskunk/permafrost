1c743bec5fd944293d8183038f4b9576b1f65f3c
Brian J Weis <brian@brianweis.net>
2019-11-24 06:55:04 -0700

    basic thermastat

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import time
 from w1thermsensor import W1ThermSensor
+from gpiozero import LED
 sensor = W1ThermSensor()
+led = LED(18)
+
+cooling = True
+idealTemp = 22
+
+
+def toggleSwitch():
+    global cooling
+
+    if cooling:
+        cooling = False
+        led.off()
+    else:
+        cooling = True
+        led.on()
+
 
 while True:
     temperature = sensor.get_temperature()
     print("The temperature is %s celsius" % temperature)
+    if temperature > idealTemp:
+        print("Temp Higher Than % so cooling" % idealTemp)
+        led.on()
+    elif temperature < idealTemp:
+        print("Temp Lower Than % stopping cooling" % idealTemp)
+        led.off()
+    # toggleSwitch()
     time.sleep(1)
