ff1b3d8a5e853f1edcdf19437faec312d2f403bc
Brian J Weis <brian@brianweis.net>
2019-11-24 08:18:25 -0700

    Moved output to single line, added buffer ability
    Temp also reads from DB now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # permafrost
+
+## Setting Temp
+sqlite3 permafrost_db.db
+UPDATE config SET value = 50 WHERE key = 'desired_temp';
--- app.py
+++ app.py
@@ -1,32 +1,79 @@
 import time
 from w1thermsensor import W1ThermSensor
 from gpiozero import LED
+from os import system, name
+import sqlite3
+
+
 sensor = W1ThermSensor()
 led = LED(18)
 
-cooling = True
-idealTemp = 22
+cooling = False
+# idealTemp = 66
+tempBuffer = .1
+
+# Connect to DB
+conn = sqlite3.connect('permafrost_db.db')
+c = conn.cursor()
 
 
-def toggleSwitch():
+def convertTempToFreedom(temp):
+    return ((temp * 9 / 5) + 32)
+
+
+def switchCooling(isOn):
     global cooling
 
-    if cooling:
-        cooling = False
-        led.off()
-    else:
+    if isOn and not cooling:
         cooling = True
         led.on()
+    elif not isOn and cooling:
+        cooling = False
+        led.off()
+
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+def updateScreen():
+    global cooling
+
+    action = "Waiting"
+    if cooling:
+        action = "Cooling"
+
+    clear()
+    print("Current Temp: %s f" % temperature)
+    print("Desired Temp: %s f" % idealTemp)
+    print("Current Action: %s " % action)
+
+
+def readTempSetting():
+    c.execute('select * from config where "key" == "desired_temp"')
+    r = c.fetchone()
+    return r[1]
 
 
 while True:
-    temperature = sensor.get_temperature()
-    print("The temperature is %s celsius" % temperature)
-    if temperature > idealTemp:
-        print("Temp Higher Than % so cooling" % idealTemp)
-        led.on()
+    # Collect Desired Temp
+    idealTemp = readTempSetting()
+    # COllect Actual Temp
+    temperature = convertTempToFreedom(sensor.get_temperature())
+
+    highRange = idealTemp + tempBuffer
+    lowRange = idealTemp - tempBuffer
+
+    if temperature > highRange:
+        switchCooling(True)
     elif temperature < idealTemp:
-        print("Temp Lower Than % stopping cooling" % idealTemp)
-        led.off()
-    # toggleSwitch()
+        switchCooling(False)
+
+    updateScreen()
     time.sleep(1)
